a07db6e01251d7ceed302c5ed7ec34f11e127134
rishika-g31 <rishikagupta312004@gmail.com>
2024-07-01 18:20:34 +0530

    added bcrypt package and update sign-in

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./userAuth");
 
 router.post("/sign-up", async (req, res) => {
   try {
@@ -68,6 +69,27 @@ router.post("/sign-in", async (req, res) => {
         res.status(400).json({ message: "Invalid credentials" });
       }
     });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.get("/get-user-information", authenticateToken, async (req, res) => {
+  try {
+    const { id } = req.headers;
+    const data = await User.findById(id).select("-password");
+    return res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.put("/update-address", authenticateToken, async (req, res) => {
+  try {
+    const { id } = req.headers;
+    const { address } = req.body;
+    await User.findByIdAndUpdate(id, { address: address });
+    return res.status(200).json({ message: "address updated successfully" });
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
